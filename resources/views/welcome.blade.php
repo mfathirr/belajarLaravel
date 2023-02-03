4bf8fd1c55483c32899d26616aeab602731bef18
MFathirr <muhdyathir623@gmail.com>
2023-02-03 10:19:56 +0700

    add middleware

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,11 @@
 @section('main')
 <div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 380px;">
         <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
+            @if (Auth::user())
+            <span class="fs-5 fw-semibold">Welcome, {{Auth::user()->name}}</span>
+            @else
             <span class="fs-5 fw-semibold">Welcome Task Manager</span>
+            @endif
         </div>
     </div>
 @endsection
